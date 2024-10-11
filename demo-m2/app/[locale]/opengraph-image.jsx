221b6b9d5060bb1746ceaef5a98e807424c59a0e
Kamran Ayub <kamran@hey.com>
2024-10-11 13:29:24 -0500

    Add dynamic og image

--- demo-m2/app/[locale]/opengraph-image.jsx
+++ demo-m2/app/[locale]/opengraph-image.jsx
@@ -1,15 +1,85 @@
-import fs from 'node:fs';
-import { NextResponse } from 'next/server';
+import fs from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { ImageResponse } from 'next/og';
+import { getTranslations } from 'next-intl/server';
 
-export const contentType = 'image/png';
+const size = { width: 1200, height: 630 };
+const interSemiBold = fs.readFile(resolve('./assets/Inter-SemiBold.ttf'));
+const interLight = fs.readFile(resolve('./assets/Inter-Light.ttf'));
 
-export default function OpenGraphImage({ params: { locale } }) {
-  const exists = fs.existsSync(`app/[locale]/opengraph-image.${locale}.png`);
-  if (exists) {
-    const buffer = fs.readFileSync(`app/[locale]/opengraph-image.${locale}.png`);
-    return new NextResponse(buffer);
-  } else {
-    const buffer = fs.readFileSync(`app/opengraph-image.png`);
-    return new NextResponse(buffer);
-  }
+export async function generateImageMetadata({ params: { locale } }) {
+  const t = await getTranslations({ locale, namespace: 'OpenGraph' });
+
+  return [
+    {
+      id: 0,
+      alt: t('heading') + ' - ' + t('subheading'),
+      contentType: 'image/png',
+      size
+    }
+  ];
+}
+
+export default async function GET({ params: { locale } }) {
+  const t = await getTranslations({ locale, namespace: 'OpenGraph' });
+
+  const H1 = ({ children }) => (
+    <h1
+      style={{
+        fontFamily: 'Inter SemiBold',
+        fontSize: 128,
+        color: 'white'
+      }}
+    >
+      {children}
+    </h1>
+  );
+
+  const H2 = ({ children }) => (
+    <h2
+      style={{
+        fontFamily: 'Inter Light',
+        fontSize: 64,
+        color: 'white'
+      }}
+    >
+      {children}
+    </h2>
+  );
+
+  return new ImageResponse(
+    (
+      <div
+        lang={locale}
+        style={{
+          background: '#FF1675',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          paddingLeft: '50px'
+        }}
+      >
+        <H1>{t('heading')}</H1>
+        <H2>{t('subheading')}</H2>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'Inter SemiBold',
+          data: await interSemiBold,
+          style: 'normal',
+          weight: 400
+        },
+        {
+          name: 'Inter Light',
+          data: await interLight,
+          style: 'normal'
+        }
+      ]
+    }
+  );
 }
